c9ecee8910201719a72427b0fcb909cb7eb98d02
MikiraSora <mikirasora0409@126.com>
2021-01-27 09:24:46 +0800

    reinit

---
+++ L2dNativeCSharpBinder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2dNativeCSharpBinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
