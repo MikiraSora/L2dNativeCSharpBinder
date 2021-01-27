d70240183d5f37fd93e4085a645f8b7ade4bd753
MikiraSora <mikirasora0409@126.com>
2021-01-27 18:13:47 +0800

    fix

--- L2dNativeCSharpBinder/Library/FrameworkLibrary.cs
+++ L2dNativeCSharpBinder/Library/FrameworkLibrary.cs
@@ -15,6 +15,8 @@ namespace L2dNativeCSharpBinder.Library
     {
         public override string ModuleName => "libFramework";
 
+        public IEnumerable<string> Headers { get; }
+
         public override void Setup(Driver driver)
         {
             base.Setup(driver);
@@ -23,12 +25,11 @@ namespace L2dNativeCSharpBinder.Library
             var module = options.AddModule(ModuleName);
 
             module.Headers.Add(@"E:\live2dProjects\Live2D-Raspbian-Native-Sample\Framework\src\CubismFramework.hpp");
-            module.CodeFiles.Add(@"E:\live2dProjects\Live2D-Raspbian-Native-Sample\Framework\src\CubismFramework.hpp");
 
             module.IncludeDirs.AddRange(new[]{
                 @"E:\live2dProjects\Live2D-Raspbian-Native-Sample\Samples\OpenGL\thirdParty\glew-2.1.0\include",
                 @"E:\live2dProjects\Live2D-Raspbian-Native-Sample\Samples\OpenGL\thirdParty\glfw-3.3\include",
-                @"E:\live2dProjects\Live2D-Raspbian-Native-Sample\Framework\src",
+                //@"E:\live2dProjects\Live2D-Raspbian-Native-Sample\Framework\src",
                 @"E:\live2dProjects\Live2D-Raspbian-Native-Sample\Core\include"
                 });
             
